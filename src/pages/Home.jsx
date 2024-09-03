568abd7f98cbf9890ebc9a2dd40685451f975ffa
zeal175 <rakeshkarthik14@gmail.com>
2024-09-03 21:59:31 +0530

    Events

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => {
   return (
     <main className='relative bg-arcane-primary'>
-        <section style={{ background: "url('/images/hero.png') no-repeat center center/cover"}} className='h-screen w-full'>
+        <section style={{ background: "url('/images/hero.png') no-repeat center center/cover"}} className='w-full h-screen'>
             
         </section>
     </main>
