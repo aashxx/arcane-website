b039060399ce5ae4559f4eae4bd0c15df0e77186
Mohamed Aashir <tmohamedaashir@gmail.com>
2024-09-05 15:27:30 +0530

    oj

--- src/pages/Clubs.jsx
+++ src/pages/Clubs.jsx
@@ -15,7 +15,7 @@ const Clubs = () => {
             <motion.div key={index} {...ANIMATIONS.up} className='md:w-[30%] h-[150px] w-full'>
               <Link to={`/clubs/${club.name}`} className="relative flex w-full max-h-hull items-center justify-start group">
                 <img src={club.img} alt={club.name} className="w-full h-[150px] rounded-lg object-contain" />
-                <div className="absolute inset-0 transition-opacity duration-300 rounded-lg md:opacity-0 bg-gradient-to-t from-arcane-primary to-transparent md:group-hover:opacity-60" />
+                <div className="absolute inset-0 transition-opacity duration-300 rounded-lg opacity-0 bg-gradient-to-t from-arcane-primary to-transparent md:group-hover:opacity-60" />
                 {/* <h4 className="absolute z-10 text-2xl md:px-2 text-white md:opacity-0 transition-opacity duration-300 font-valorax bottom-4 left-4 md:group-hover:opacity-100">
                   {club.name}
                 </h4> */}
