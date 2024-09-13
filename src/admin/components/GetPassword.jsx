0c339779c5149bb5e566e86a509888c707f996c3
Mohamed Aashir <tmohamedaashir@gmail.com>
2024-09-13 23:34:08 +0530

    done

--- src/admin/components/GetPassword.jsx
+++ src/admin/components/GetPassword.jsx
@@ -4,6 +4,13 @@ import { Input } from '@/components/ui/input';
 import React from 'react';
 
 const GetPassword = ({ password, setPassword, authenticate }) => {
+
+    const handleAuthenticate = (event) => {
+        if(event.key === 'Enter') {
+            authenticate();
+        }
+    }
+
     return (
         <div className='text-center text-white font-expletus-sans'>
             <Dialog open>
@@ -13,7 +20,7 @@ const GetPassword = ({ password, setPassword, authenticate }) => {
                             Enter Password
                         </DialogTitle>
                     </DialogHeader>
-                    <Input type={'password'} value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Enter password here" className="mx-auto md:w-full w-full" />
+                    <Input onKeyDown={handleAuthenticate} type={'password'} value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Enter password here" className="mx-auto md:w-full w-full" />
                     <DialogFooter>
                         <Button onClick={authenticate} className={'w-[150px]'}>
                             Enter
